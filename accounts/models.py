d0a42ee60a61188ca3628d574e3f6eb072496c64
jelenu <jesusleon2700@gmail.com>
2025-06-10 11:07:55 +0200

    add username to user model

--- accounts/models.py
+++ accounts/models.py
@@ -2,28 +2,31 @@ from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseU
 from django.db import models
 
 class CustomUserManager(BaseUserManager):
-    def create_user(self, email, password=None, **extra_fields):
+    def create_user(self, email, username, password=None, **extra_fields):
         if not email:
             raise ValueError("User must have an email address")
+        if not username:
+            raise ValueError("User must have a username")
         email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
+        user = self.model(email=email, username=username, **extra_fields)
         user.set_password(password)
         user.save()
         return user
 
-    def create_superuser(self, email, password=None, **extra_fields):
+    def create_superuser(self, email, username, password=None, **extra_fields):
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
-        return self.create_user(email, password, **extra_fields)
+        return self.create_user(email, username, password, **extra_fields)
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True)
+    username = models.CharField(max_length=150, unique=True)
     is_active = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
     date_joined = models.DateTimeField(auto_now_add=True)
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = ['username']
 
     objects = CustomUserManager()
 
--- accounts/serializers.py
+++ accounts/serializers.py
@@ -3,15 +3,21 @@ from .models import CustomUser
 from django.contrib.auth.password_validation import validate_password
 
 class RegisterSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(required=True)
     password = serializers.CharField(write_only=True, validators=[validate_password])
 
     class Meta:
         model = CustomUser
-        fields = ('email', 'password')
+        fields = ('email', 'username', 'password')
+    def validate_username(self, value):
+        if CustomUser.objects.filter(username=value).exists():
+            raise serializers.ValidationError("Username alredy exists")
+        return value
 
     def create(self, validated_data):
         user = CustomUser.objects.create_user(
             email=validated_data['email'],
+            username=validated_data['username'],
             password=validated_data['password']
         )
         user.is_active = False
