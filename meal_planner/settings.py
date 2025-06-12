e413cd0f4a0bf9f4934c474f52fd74c375732edb
jelenu <jesusleon2700@gmail.com>
2025-06-12 11:44:25 +0200

    recipe model created

--- meal_planner/settings.py
+++ meal_planner/settings.py
@@ -35,7 +35,8 @@ INSTALLED_APPS = [
     'drf_spectacular',
     
     'accounts',
-]
+    'recipes',
+    ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
---
+++ recipes/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.conf import settings
+
+class Recipe(models.Model):
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='recipes'
+    )
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    image = models.ImageField(upload_to='recipes/', blank=True, null=True)
+    preparation_time = models.PositiveIntegerField(help_text="Time in minutes")
+    ingredients = models.TextField(help_text="Free text or list of ingredients")
+    steps = models.TextField(help_text="Recipe steps")
+    category = models.CharField(max_length=100, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_public = models.BooleanField(default=False, help_text="Is this recipe visible to everyone?")
+
+    def __str__(self):
+        return self.title
